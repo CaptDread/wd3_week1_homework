135ccf4a1c6920be50aef977152cd268920585c7
CaptDread <domlycaon@gmail.com>
2021-01-10 15:53:00 -0500

    whatHellRevert

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,7 +8,7 @@ for (let z=0; z < spanFun.length; z++){
         let g = Math.random() * 200 + 75 
         let b = Math.random() * 200 + 75
         spanFun[z].style.color = `rgb(${r}, ${g}, ${b})`
-        spanFUn[z].style.transition = `color 2s`
+        spanFun[z].style.transition = `color 2s`
     }
     setInterval(spanFuninator, 420)
 }
